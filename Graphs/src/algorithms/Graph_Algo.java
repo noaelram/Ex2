3a398b20f18e4aff33d85d49320c101633bf820b
noaelram <noaelram@hotmail.com>
2019-12-24 22:32:40 +0200

    more function adding commit

--- Graphs/src/algorithms/Graph_Algo.java
+++ Graphs/src/algorithms/Graph_Algo.java
@@ -5,7 +5,15 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
 
 import dataStructure.DGraph;
 import dataStructure.edge_data;
@@ -84,4 +92,172 @@ public class Graph_Algo implements graph_algorithms {
 			e1.printStackTrace();
 		}
 	}
+	
+	@Override
+	public boolean isConnected() {
+		for (node_data n : g.getV()) {
+			if (reachableFrom(n.getKey()).size() != this.g.nodeSize()) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public double shortestPathDist(int src, int dest) {
+		if (src == dest) {
+			return 0;
+		}
+		List<node_data> path = shortestPath(src, dest);
+		if (path == null) {
+			return -1;
+		}
+		return path.get(path.size() - 1).getWeight();
+	}
+	
+	// ASSUMPTION: computeShortestPaths(src) run before calling this function
+		private List<node_data> shortestPathAux(int src, int dest) {
+			List<node_data> path = new ArrayList<node_data>();
+			if (src == dest) {
+				path.add(g.getNode(src));
+				return path;
+			}
+			int key = dest;
+			while (key != src && key != -1) {
+				path.add(g.getNode(key));
+				key = g.getNode(key).getTag();
+			}
+			if (key == -1) {
+				return null;
+			}
+			path.add(g.getNode(src));
+			Collections.reverse(path);
+			return path;
+		}
+		
+		@Override
+		public List<node_data> TSP(List<Integer> targets) {
+			HashMap<Integer, HashMap<Integer, Double>> shortestPathsValue = new HashMap<Integer, HashMap<Integer, Double>>();
+			HashMap<Integer, HashMap<Integer, List<node_data>>> shortestPathsNodes = new HashMap<Integer, HashMap<Integer, List<node_data>>>();
+			for (int src : targets) {
+				computeShortestPaths(src);
+				HashMap<Integer, Double> shortestPathsValuesFromSource = new HashMap<Integer, Double>();
+				HashMap<Integer, List<node_data>> shortestPathsNodesFromSource = new HashMap<Integer, List<node_data>>();
+				for (int dest : targets) {
+					if (dest == src)
+						continue;
+					// set min distance from src to dest
+					shortestPathsValuesFromSource.put(dest, g.getNode(dest).getWeight());
+					shortestPathsNodesFromSource.put(dest, shortestPathAux(src, dest));
+				}
+				shortestPathsValue.put(src, shortestPathsValuesFromSource);
+				shortestPathsNodes.put(src, shortestPathsNodesFromSource);
+			}
+			targets.stream().toArray();
+			Integer[] targetsArr = targets.stream().toArray(Integer[]::new);
+			Integer[] shortest = new Integer[targetsArr.length];
+			findShortest(targetsArr, shortest, shortestPathsValue);
+			List<node_data> finalPath = new ArrayList<node_data>();
+			int from;
+			int to;
+			for (int i = 0; i < shortest.length - 1; i++) {
+				from = shortest[i];
+				to = shortest[i + 1];
+				List<node_data> nodes = shortestPathsNodes.get(from).get(to);
+				if (nodes == null) {
+					return null;
+				}
+				for (int j = 0; j < nodes.size(); j++) {
+					if (j == nodes.size() - 1 && i != shortest.length - 2) {
+						continue;
+					}
+					node_data n = nodes.get(j);
+					finalPath.add(n);
+				}
+			}
+			return finalPath;
+		}
+		
+		private static void swap(Integer[] t, int i, int j) {
+			int temp = t[i];
+			t[i] = t[j];
+			t[j] = temp;
+		}
+		
+		private double findShortest(Integer[] targetsArr, Integer[] shortestPath,
+				HashMap<Integer, HashMap<Integer, Double>> shortestPathsValue) {
+			return findShortestAux(targetsArr, 0, Double.MAX_VALUE, shortestPath, shortestPathsValue);
+		}
+		
+		private double findShortestAux(Integer[] targetsArr, int currentIndex, double shortest, Integer[] shortestPath,
+				HashMap<Integer, HashMap<Integer, Double>> shortestPathsValue) {
+			if (currentIndex == targetsArr.length - 1) {
+				double value = calculatePath(targetsArr, shortestPathsValue);
+				if (value < shortest) {
+					copyArray(targetsArr, shortestPath);
+					return value;
+				}
+				return shortest;
+			}
+
+			for (int i = currentIndex; i < targetsArr.length; i++) {
+				swap(targetsArr, currentIndex, i);
+				double value = findShortestAux(targetsArr, currentIndex + 1, shortest, shortestPath, shortestPathsValue);
+				swap(targetsArr, currentIndex, i);
+			}
+
+			return shortest;
+		}
+		
+		private static void copyArray(Integer[] targetsArr, Integer[] shortestPath) {
+			for (int i = 0; i < targetsArr.length; i++) {
+				shortestPath[i] = targetsArr[i];
+			}
+		}
+		
+		private double calculatePath(Integer[] targetsArr, HashMap<Integer, HashMap<Integer, Double>> shortestPathsValue) {
+			double sum = 0;
+			for (int i = 0; i < targetsArr.length - 1; i++) {
+				int from = targetsArr[i];
+				int to = targetsArr[i + 1];
+				double distance = shortestPathsValue.get(from).get(to);
+				sum += distance;
+			}
+			return sum;
+		}
+		
+		@Override
+		public graph copy() {
+			DGraph newGraph = new DGraph();
+			for (node_data n : g.getV()) {
+				newGraph.addNode(new node(n));
+				for (edge_data e : g.getE(n.getKey())) {
+					newGraph.connect(e.getSrc(), e.getDest(), e.getWeight());
+				}
+			}
+			return newGraph;
+		}
+		
+		
+	
+	// run a DFS to find all reachable vertices
+		private Set<Integer> reachableFrom(int src) {
+			Set<Integer> visited = new HashSet<Integer>();
+			Queue<Integer> queue = new LinkedList<Integer>();
+			queue.add(src);
+			while (queue.size() > 0) {
+				int current = queue.poll();
+				Collection<edge_data> edges = this.g.getE(current);
+				for (edge_data edge : edges) {
+					int dest = edge.getDest();
+					if (!visited.contains(dest)) {
+						visited.add(dest);
+						queue.add(dest);
+					}
+				}
+			}
+			
+			return visited;
+		}
+
 }
